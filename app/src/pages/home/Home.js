2194e6835f556a60a216dc95f991a3eef2af54b7
Gino Villalpando <grievxus@outlook.com>
2020-05-25 15:24:25 -0600

    added search box that searches the businesses list

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,30 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 
-import {ProfileList} from "../profile/ProfileList";
-// import {getAllProfiles, getProfileByEmail} from "../../shared/actions/get-profile";
-import Grid from '@material-ui/core/Grid';
-import axios from 'axios';
-import Profile from '../profile/Profile'
+// import {ProfileList} from "../profile/ProfileList";
+// // import {getAllProfiles, getProfileByEmail} from "../../shared/actions/get-profile";
+// import Grid from '@material-ui/core/Grid';
+// import axios from 'axios';
+// import Profile from '../profile/Profile'
 
 import {getBusinessesByBusinessName, getTopBusinesses} from "../../shared/actions/get-businesses";
-import {BusinessList} from "../business/BusinessList";
+// import {BusinessList} from "../business/BusinessList";
 import {getAllBehaviors} from "../../shared/actions/get-behaviors";
-import {TestBusiness} from "../business/TestBusiness";
+import {Business} from "../business/Business";
 import {getAllVotes} from "../../shared/actions/get-votes";
-import profileReducer from '../../shared/reducers/profileReducer';
-import {Button, Form, FormControl} from "react-bootstrap";
+// import profileReducer from '../../shared/reducers/profileReducer';
+// import {Button, Form, FormControl} from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import { SearchFormContent } from '../../shared/components/SearchForm/SearchForm';
 
 export const Home= () => {
 
+	const [searchWord, setSearchWord] = useState('');
+
 	// use selector to set users to users stored in state
-	const businesses = useSelector(state => state.businesses ? state.businesses : []);
+	const businessState = useSelector(state => state.businesses ? state.businesses : []);
 	const behaviors = useSelector(state => state.behaviors ? state.behaviors : []);
 	const votes = useSelector(state => state.votes ? state.votes : [])
 
+	const filterBusiness = businessState.filter(
+		businesses => businesses.businessName.includes(searchWord) || businesses.businessUrl.includes(searchWord)
+	);
+
 	// use dispatch from redux to dispatch actions
-	const dispatch = useDispatch();
+	const businesses = filterBusiness, dispatch = useDispatch();
 
 	// get profiles
 	const effects = () => {
@@ -49,17 +56,12 @@ export const Home= () => {
 	return (
 		<main className="container">
 			<Navbar bg="white" variant="dark">
-				<Form inline>
-					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-				</Form>
-				<Form inline>
-					<Button variant="outline-info" onClick={search}>Search</Button>
-				</Form>
+				<SearchFormContent searchWord={searchWord} setSearchWord={setSearchWord}/>
 			</Navbar>
 
 			{
 				businesses.map(
-			business => <TestBusiness key={business.businessId} business={business}  behaviors={behaviors.filter(behavior =>
+			business => <Business key={business.businessId} business={business} searchWord={searchWord} behaviors={behaviors.filter(behavior =>
 				behavior.behaviorBusinessId === business.businessId
 			)} 
 
